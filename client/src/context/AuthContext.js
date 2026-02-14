d680ff5298b8dbe5faa038bedb7e05d1144cb282
saikiranbeesa <saikiranbeesa@gmail.com>
2026-02-15 00:50:27 +0530

    api updation

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
   const register = useCallback(async (name, email, password) => {
     try {
       setError(null);
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://bellcorp-event-assignment.onrender.com/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ export const AuthProvider = ({ children }) => {
   const login = useCallback(async (email, password) => {
     try {
       setError(null);
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://bellcorp-event-assignment.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
